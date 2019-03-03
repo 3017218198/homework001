2cb7e3a0e751c93902eeaca95cb399ad07458dca
3017218198 <lljlljly@tju.edu.cn>
2019-03-03 11:02:55 +0800

    second edition

---
+++ homework001(branch01)/homework001(branch01)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework001_branch01_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Green;
+            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine("Hello World!");
+            /*
+             * use method ResetColor to reset the background and foreground color
+             */
+            Console.ResetColor();
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
